edc224d8f0d89dd0c905ca7e4cc9fac6fee85ce3
arismil <aris.miletis@icloud.com>
2020-05-22 02:03:01 +0300

    start

--- arislist/models.py
+++ arislist/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 
 # Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Classified(models.Model):
+    Name = models.CharField(max_length=200)
+    Model_Name = models.CharField(max_length=10)
+    Date_Created = models.DateTimeField('Date published')
+    Price = models.IntegerField(default=0)
+
+
+class Product(models.Model):
+    Model_Name = models.ForeignKey(Classified.Model_Name)
+    # Version = models.
